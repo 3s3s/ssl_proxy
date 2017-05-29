8aaa94acb4fc64df2743251eab1c5655e52d9441
3s3s <ivanivanovkzv@gmail.com>
2017-05-29 11:13:31 +0000

    fix ssl for letsencrypt

--- constants.js
+++ constants.js
@@ -7,12 +7,20 @@ exports.g_bDebug = process.env.PORT ? true : false;
 exports.my_portHTTPS = exports.g_bDebug ? 4443 : 443; //process.env.PORT || 443; //4443;
 exports.my_portHTTP = process.env.PORT || 80;
 
+const cert = '/etc/letsencrypt/live/ppc.multicoins.org-0001/fullchain.pem';
+const key = '/etc/letsencrypt/live/ppc.multicoins.org-0001/privkey.pem';
+//const cert = '/root/ssl_proxy/ssl_cert/server.crt';
+//const key = '/root/ssl_proxy/ssl_cert/server.key';
+
 const domains = [
-    {'name' : 'langtest.ru', 'port' : '8088', 'ssl' : 'false', 'keyname' : 'server'}, 
-    {'name' : 'multicoins.org', 'port' : '9443', 'ssl' : 'true', 'keyname' : 'server'},
-    {'name' : 'trade.multicoins.org', 'port' : '11443', 'ssl' : 'true', 'keyname' : 'server'},
-    {'name' : 'natcoin.multicoins.org', 'port' : '14443', 'ssl' : 'true', 'keyname' : 'server'},
-    {'name' : 'ppc.multicoins.org', 'port' : '15443', 'ssl' : 'true', 'keyname' : 'server'}
+//    {'name' : 'langtest.ru', 'port' : '8088', 'ssl' : 'false', 'keyname' : 'server'}, 
+//    {'name' : 'multicoins.org', 'port' : '9443', 'ssl' : 'true', 'keyname' : 'server'},
+//    {'name' : 'trade.multicoins.org', 'port' : '11443', 'ssl' : 'true', 'keyname' : 'server'},
+//    {'name' : 'natcoin.multicoins.org', 'port' : '14443', 'ssl' : 'true', 'keyname' : 'server'},
+    {'name' : 'ppc.multicoins.org', 'port' : '15443', 'ssl' : 'true'},
+    {'name' : 'ppc-ex.multicoins.org', 'port' : '11443', 'ssl' : 'true'},
+    {'name' : 'mc.multicoins.org', 'port' : '16443', 'ssl' : 'true'},
+    {'name' : 'mc-ex.multicoins.org', 'port' : '17443', 'ssl' : 'true'}
 ];
 
 /////////////////////////////////////////////////////////////
@@ -28,10 +36,10 @@ exports.GetHostAndPort2 = function(host)
     return domains[0];
 }
 
-function getSecureContext (filename) {
+function getSecureContext (keyname, certname) {
     return tls.createSecureContext({
-        key:  fs.readFileSync(__dirname + "/ssl_cert/" + filename + ".key"),
-        cert: fs.readFileSync(__dirname + "/ssl_cert/" + filename + ".crt")
+        key:  fs.readFileSync(keyname),
+        cert: fs.readFileSync(certname)
       }).context;
 }
 
@@ -39,7 +47,7 @@ function getSecureContext (filename) {
 const secureContext = function(domains) {
         var ret = {};
         for (var i=0; i<domains.length; i++)
-            ret[domains[i].name] = getSecureContext(domains[i].keyname);
+            ret[domains[i].name] = getSecureContext(key, cert)//(domains[i].keyname);
         
         return ret;
     }(domains);
@@ -57,8 +65,8 @@ exports.options = {
             throw new Error('No keys/certificates for domain requested');
         }
     }, 
-    key: fs.readFileSync(__dirname + "/ssl_cert/server.key"),
-    cert: fs.readFileSync(__dirname + "/ssl_cert/server.crt")
+    key: fs.readFileSync(key),
+    cert: fs.readFileSync(cert)
 }
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
